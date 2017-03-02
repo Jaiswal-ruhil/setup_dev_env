a392576b986c04c01f284e6a8f2853e5d9943c8c
Ruhil Jaiswal <ruhiljaiswal@gmail.com>
2017-03-02 22:57:08 +0530

    bug fix: flag file check for dev-env command

--- scripts/cmd.sh
+++ scripts/cmd.sh
@@ -9,8 +9,7 @@ dev_run() {
 exec_flag() {
     local FLAG=$1
     local VALUE=$2
-    temp=`check_file $_REPO_DIR_/scripts/flags/$FLAG.sh`
-    if [[ $temp -eq 0  ]]; then
+    if [[ -e $_REPO_DIR_/scripts/flags/$FLAG.sh  ]]; then
         bash $_REPO_DIR_/scripts/flags/$FLAG.sh $_REPO_DIR_ $VALUE
     else
         bash $CALL_ORIGIN/$REPO_DIR_NAME/$FLAGS_FOLDER_NAME/$FLAG.sh $VALUE
